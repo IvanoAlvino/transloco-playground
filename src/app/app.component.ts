f9257a2061eba1d4c0064c9aa2ed2bc758320394
Ivano Alvino <ivano.alvino@navvis.com>
2019-10-23 17:04:15 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TranslocoService} from '@ngneat/transloco';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'transloco-playground';
+
+  params = {link: 'ADMIN', pwd: 'BANANA'};
+
+  private result: string;
+
+  constructor(private translocoService: TranslocoService) {
+  }
+
+  public ngOnInit(): void {
+    this.translocoService.selectTranslate('This comes from the component {value}', {value: 'HELLO WORLD'})
+        .subscribe(value => this.result = value);
+  }
+
+  public switchToSpanish() {
+    this.translocoService.setActiveLang('es');
+  }
+
+  public switchToEnglish() {
+    this.translocoService.setActiveLang('en');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
+import { translocoLoader } from './transloco.loader';
+import { TranslocoModule, TRANSLOCO_CONFIG, TranslocoConfig } from '@ngneat/transloco';
+import { TranslocoMessageFormatModule } from '@ngneat/transloco-messageformat';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,22 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslocoModule,
+    TranslocoMessageFormatModule.init()
   ],
-  providers: [],
+  providers: [{
+      provide: TRANSLOCO_CONFIG,
+      useValue: {
+        availableLangs: ['en', 'es'],
+        defaultLang: 'en',
+        prodMode: environment.production,
+        reRenderOnLangChange: true,
+      } as TranslocoConfig
+    },
+    translocoLoader
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
